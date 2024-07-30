a58e704dadc9d51c8196de879579115b528c6e4f
Gustavo Gonzalez <gadol.gonzalez@gmail.com>
2024-07-30 11:10:30 -0400

    fixed substring range

--- src/main/kotlin/Lexer.kt
+++ src/main/kotlin/Lexer.kt
@@ -1,11 +1,9 @@
-import kotlin.math.exp
-
 class Lexer(val source: String) {
     var start: Int = 0
     var current: Int = 0
     var line: Int = 1
 
-    private var keywords = mapOf<String,TokenType>(
+    private var keywords = mapOf<String, TokenType>(
         Pair("and", TokenType.AND),
         Pair("class", TokenType.CLASS),
         Pair("else", TokenType.ELSE),
@@ -20,12 +18,13 @@ class Lexer(val source: String) {
         Pair("super", TokenType.SUPER),
         Pair("this", TokenType.THIS),
         Pair("var", TokenType.VAR),
-        Pair("while", TokenType.WHILE))
+        Pair("while", TokenType.WHILE)
+    )
 
     private var tokens = mutableListOf<Token>()
 
-    fun scanTokens():List<Token>{
-        while(!isAtEnd()){
+    fun scanTokens(): List<Token> {
+        while (!isAtEnd()) {
             start = current
             scan()
         }
@@ -33,9 +32,9 @@ class Lexer(val source: String) {
         return tokens
     }
 
-    private fun scan(){
+    private fun scan() {
         val c = advance()
-        when(c){
+        when (c) {
             '(' -> addToken(TokenType.LEFT_PAREN)
             ')' -> addToken(TokenType.RIGHT_PAREN)
             '{' -> addToken(TokenType.LEFT_BRACE)
@@ -46,135 +45,141 @@ class Lexer(val source: String) {
             '+' -> addToken(TokenType.PLUS)
             ';' -> addToken(TokenType.SEMICOLON)
             '*' -> addToken(TokenType.STAR)
-            '!' -> addToken(if(match('=')) TokenType.BANG_EQUAL else TokenType.BANG)
-            '=' -> addToken(if(match('=')) TokenType.EQUAL_EQUAL else TokenType.EQUAL)
-            '<' -> addToken(if(match('=')) TokenType.LESS_EQUAL else TokenType.LESS)
-            '>' -> addToken(if(match('=')) TokenType.GREATER_EQUAL else TokenType.GREATER)
+            '!' -> addToken(if (match('=')) TokenType.BANG_EQUAL else TokenType.BANG)
+            '=' -> addToken(if (match('=')) TokenType.EQUAL_EQUAL else TokenType.EQUAL)
+            '<' -> addToken(if (match('=')) TokenType.LESS_EQUAL else TokenType.LESS)
+            '>' -> addToken(if (match('=')) TokenType.GREATER_EQUAL else TokenType.GREATER)
             '/' -> {
-                if(match('/')){
-                    while(peek() != '\n' && !isAtEnd()){
+                if (match('/')) {
+                    while (peek() != '\n' && !isAtEnd()) {
                         advance()
                     }
-                }else{
+                } else {
                     addToken(TokenType.SLASH)
                 }
             }
+
             ' ', '\r', '\t' -> {
                 //ignore whitespace
             }
+
             '\n' -> {
                 line++
             }
+
             '"' -> {
                 string()
             }
+
             else -> {
-                if(isDigit(c)){
+                if (isDigit(c)) {
                     number()
-                }else if(isAlpha(c)){
+                } else if (isAlpha(c)) {
                     identifier()
-                }else{
+                } else {
                     //Error
                 }
             }
         }
     }
 
-    private fun isAlpha(char: Char):Boolean{
+    private fun isAlpha(char: Char): Boolean {
         return char in 'a'..'z'
                 || char in 'A'..'Z'
                 || char == '_'
     }
 
-    private fun isDigit(c:Char):Boolean{
+    private fun isDigit(c: Char): Boolean {
         return c in '0'..'9'
     }
 
-    private fun isAlphaNumeric(c:Char):Boolean{
+    private fun isAlphaNumeric(c: Char): Boolean {
         return isAlpha(c) || isDigit(c)
     }
 
-    private fun identifier(){
-        while(isAlphaNumeric(peek())){
+    private fun identifier() {
+        while (isAlphaNumeric(peek())) {
             advance()
         }
-        var text = source.substring(start .. current)
+        var text = source.substring(start until current)
         var type = keywords[text]
-        if(type == null){
+        if (type == null) {
             type = TokenType.IDENTIFIER
         }
         addToken(type)
     }
-    private fun number(){
-        while(isDigit(peek())){
+
+    private fun number() {
+        while (isDigit(peek())) {
             advance()
         }
-        if(peek() == '.' && isDigit(peekNext())){
+        if (peek() == '.' && isDigit(peekNext())) {
             advance()
-            while(isDigit(peek())){
+            while (isDigit(peek())) {
                 advance()
             }
         }
-        val text = source.substring(start .. current)
+        val text = source.substring(start until current)
         addToken(TokenType.NUMBER, text.toDouble())
     }
-    private fun string(){
-        while(peek() != '"' && !isAtEnd()){
-            if(peek() == '\n'){
+
+    private fun string() {
+        while (peek() != '"' && !isAtEnd()) {
+            if (peek() == '\n') {
                 line++
             }
             advance()
         }
-        if(isAtEnd()){
+        if (isAtEnd()) {
             //error
             return
         }
         advance()
-        val value = source.substring(start+1..current-1)
+        val value = source.substring(start + 1 until current - 1)
         addToken(TokenType.STRING, value)
     }
 
-    private fun match(expected:Char):Boolean{
-        if(isAtEnd()){
+    private fun match(expected: Char): Boolean {
+        if (isAtEnd()) {
             return false
         }
-        if(source[current] != expected){
+        if (source[current] != expected) {
             return false
         }
         current++
         return true
     }
 
-    private fun peekNext():Char{
-        return if(current + 1 >= source.length){
-             0.toChar()
-        }else{
-            source[current+1]
+    private fun peekNext(): Char {
+        return if (current + 1 >= source.length) {
+            0.toChar()
+        } else {
+            source[current + 1]
         }
     }
 
-    private fun isAtEnd():Boolean{
+    private fun isAtEnd(): Boolean {
         return current >= this.source.length
     }
 
-    private fun advance():Char{
+    private fun advance(): Char {
         return this.source[this.current++]
     }
 
-    private fun peek():Char{
-        return if(isAtEnd()){
+    private fun peek(): Char {
+        return if (isAtEnd()) {
             0.toChar()
-        }else{
+        } else {
             source[current]
         }
     }
 
-    private fun addToken(type: TokenType){
+    private fun addToken(type: TokenType) {
         addToken(type, null)
     }
 
-    private fun addToken(type: TokenType, literal:Any?){
-        val text = source.substring(start..current)
+    private fun addToken(type: TokenType, literal: Any?) {
+        val text = source.substring(start until current)
         tokens.addLast(Token(type, text, literal, line))
     }
 }
---
+++ src/test/java/LexerTest.kt
@@ -0,0 +1,14 @@
+import org.jetbrains.annotations.TestOnly
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class LexerTest{
+    @Test
+    fun lexTest(){
+        val text = "("
+        val lexer = Lexer(text)
+        val tokens = lexer.scanTokens()
+        System.out.println(tokens)
+    }
+
+}
